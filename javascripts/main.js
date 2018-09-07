adb54bfcb3cd7ef451d33e050f8c7ec383f9d7d4
rndev80 <rndev80@gmail.com>
2018-09-07 06:30:42 -0700

    group 79, 47

--- javascripts/main.js
+++ javascripts/main.js
@@ -13,21 +13,21 @@ function group7_animation() {
         autoplay: false
     });
     
-    var transX = [50, 0, 0, 90, 30, 190];
-    var transY = [10, 0, 120, 200, 230, 0];
-    var init_scale = [1/2, 1/20, 1/2, 1/10, 1/2, 1/2];
-    var init_rotate = [-60, 0, -50, 0, 50, 50];
-    var duration = [3000, 1800, 3000, 1800, 3000, 3000];
+    var transX = [50, 0, 0, 90, 30, 190, 230, 290];
+    var transY = [10, 0, 120, 200, 230, 0, 90, 40];
+    var init_scale = [1/2, 1/20, 1/2, 1/10, 1/2, 1/2, 1/10, 1/4];
+    var init_rotate = [-60, 0, -50, 0, 50, 90, 0, -80];
+    var duration = [3000, 1800, 3000, 1800, 3000, 3000, 3000, 3000];
     
     timeline.add({
-        targets: ['#Group-41', '#Fill-49', '#Group-43', '#Group-48', '#Group-44', '#Group-46'],
+        targets: ['#Group-41', '#Fill-49', '#Group-43', '#Group-48', '#Group-44', '#Group-46', '#Group-79', '#Group-47'],
         translateX: 160,
         translateY: 250,
         scale: function(el, i) { return init_scale[i]; },
         rotate: function(el, i) { return init_rotate[i]; },
         duration: 0
     }).add({
-        targets: ['#Group-41', '#Fill-49', '#Group-43', '#Group-48', '#Group-44', '#Group-46'],
+        targets: ['#Group-41', '#Fill-49', '#Group-43', '#Group-48', '#Group-44', '#Group-46', '#Group-79', '#Group-47'],
         translateX: function(el, i) { return transX[i]; },
         translateY: function(el, i) { return transY[i]; },
         scale: 1,
@@ -36,20 +36,20 @@ function group7_animation() {
         duration: function(el, i) { return duration[i]; },
     });
     
-    var init_transX2 = [50, 0, 0, 90, 30, 190];
-    var init_transY2 = [10, 0, 120, 200, 230, 0];
-    var scale = [1/2, 1/10, 1/2, 1/10, 1/2, 1/2];
-    var rotate = [-60, 0, -50, 0, 50, 50];
-    var duration = [2500, 3000, 2500, 3000, 2500, 2500];
+    var init_transX2 = [50, 0, 0, 90, 30, 190, 230, 290];
+    var init_transY2 = [10, 0, 120, 200, 230, 0, 90, 40];
+    var scale = [1/2, 1/10, 1/2, 1/10, 1/2, 1/2, 1/10, 1/4];
+    var rotate = [-60, 0, -50, 0, 50, 90, 0, -80];
+    var duration = [2500, 3000, 2500, 3000, 2500, 2500, 2500, 2500];
     
     timeline2.add({
-        targets: ['#Group-41', '#Fill-49', '#Group-43', '#Group-48', '#Group-44', '#Group-46'],
+        targets: ['#Group-41', '#Fill-49', '#Group-43', '#Group-48', '#Group-44', '#Group-46', '#Group-79', '#Group-47'],
         translateX: function(el, i) { return init_transX2[i]; },
         translateY: function(el, i) { return init_transY2[i]; },
         rotate: 0,
         duration: 0
     }).add({
-        targets: ['#Group-41', '#Fill-49', '#Group-43', '#Group-48', '#Group-44', '#Group-46'],
+        targets: ['#Group-41', '#Fill-49', '#Group-43', '#Group-48', '#Group-44', '#Group-46', '#Group-79', '#Group-47'],
         translateX: 160,
         translateY: 250,
         scale: function(el, i) { return scale[i]; },
