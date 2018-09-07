f19dfb413335a4d4ec0a77a8db597deec12c66cf
rndev80 <rndev80@gmail.com>
2018-09-07 05:40:29 -0700

    group-48

--- javascripts/main.js
+++ javascripts/main.js
@@ -13,44 +13,43 @@ function group7_animation() {
         autoplay: false
     });
     
-    var transX = [50, 0, 0];
-    var transY = [10, 0, 120];
-    var init_scale = [1/2, 1/20, 1/2];
-    var init_rotate = [-60, 0, -50];
-    var rotate = [0, 0, 0];
-    var duration = [3000, 1800, 3000];
+    var transX = [50, 0, 0, 90];
+    var transY = [10, 0, 120, 200];
+    var init_scale = [1/2, 1/20, 1/2, 1/10];
+    var init_rotate = [-60, 0, -50, 0];
+    var duration = [3000, 1800, 3000, 1800];
     
     timeline.add({
-        targets: ['#Group-41', '#Fill-49', '#Group-43'],
+        targets: ['#Group-41', '#Fill-49', '#Group-43', '#Group-48'],
         translateX: 160,
         translateY: 250,
         scale: function(el, i) { return init_scale[i]; },
         rotate: function(el, i) { return init_rotate[i]; },
         duration: 0
     }).add({
-        targets: ['#Group-41', '#Fill-49', '#Group-43'],
+        targets: ['#Group-41', '#Fill-49', '#Group-43', '#Group-48'],
         translateX: function(el, i) { return transX[i]; },
         translateY: function(el, i) { return transY[i]; },
         scale: 1,
-        rotate: function(el, i) { return rotate[i]; },
+        rotate: 0,
         easing: 'easeOutExpo',
         duration: function(el, i) { return duration[i]; },
     });
     
-    var init_transX2 = [50, 0, 0];
-    var init_transY2 = [10, 0, 120];
-    var scale = [1/2, 1/10, 1/2];
-    var rotate = [-60, 0, -50];
-    var duration = [2500, 3000, 2500];
+    var init_transX2 = [50, 0, 0, 90];
+    var init_transY2 = [10, 0, 120, 200];
+    var scale = [1/2, 1/10, 1/2, 1/10];
+    var rotate = [-60, 0, -50, 0];
+    var duration = [2500, 3000, 2500, 3000];
     
     timeline2.add({
-        targets: ['#Group-41', '#Fill-49', '#Group-43'],
+        targets: ['#Group-41', '#Fill-49', '#Group-43', '#Group-48'],
         translateX: function(el, i) { return init_transX2[i]; },
         translateY: function(el, i) { return init_transY2[i]; },
         rotate: 0,
         duration: 0
     }).add({
-        targets: ['#Group-41', '#Fill-49', '#Group-43'],
+        targets: ['#Group-41', '#Fill-49', '#Group-43', '#Group-48'],
         translateX: 160,
         translateY: 250,
         scale: function(el, i) { return scale[i]; },
