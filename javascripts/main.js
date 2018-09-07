99fbe8f92b6f1b65111fc8f0af166e422ed183ab
rndev80 <rndev80@gmail.com>
2018-09-07 05:06:08 -0700

    dot

--- javascripts/main.js
+++ javascripts/main.js
@@ -3,51 +3,57 @@ var timeline = anime.timeline({
     autoplay: true
 });
 
-var timeline1 = anime.timeline({
+var timeline2 = anime.timeline({
     loop: false,
     autoplay: false
 });
 
+var translateX = [50, 0];
+var translateY = [10, 0];
+var scale = [1/2, 1/20];
+var init_rotate = [-60, 0];
+var rotate = [0, 0];
+var duration = [3000, 1800];
+
 timeline.add({
-    targets: '#Group-41',
+    targets: ['#Group-41', '#Fill-49'],
     translateX: 160,
     translateY: 250,
-    scale: 1/2,
-    rotate: -60,
+    scale: function(el, i) { return scale[i]; },
+    rotate: function(el, i) { return init_rotate[i]; },
     duration: 0
 }).add({
-    targets: '#Group-41',
-    translateX: 50,
-    translateY: 10,
-    scale: {
-        value: 1,
-        duration: 2500
-    },
-    rotate: {
-        value: 0,
-        duration: 2500
-    },
+    targets: ['#Group-41', '#Fill-49'],
+    translateX: function(el, i) { return translateX[i]; },
+    translateY: function(el, i) { return translateY[i]; },
+    scale: 1,
+    rotate: function(el, i) { return rotate[i]; },
     easing: 'easeOutExpo',
-    duration: 2500
+    duration: function(el, i) { return duration[i]; },
 });
 
-timeline1.add({
-    targets: '#Group-41',
-    translateX: 50,
-    translateY: 10,
+var init_translateX2 = [50, 0];
+var init_translateY2 = [10, 0];
+var translateX2 = [160, 160];
+var translateY2 = [250, 250];
+var scale = [1/2, 1/10];
+var rotate = [-60, 0];
+var duration = [2500, 3000];
+
+timeline2.add({
+    targets: ['#Group-41', '#Fill-49'],
+    translateX: function(el, i) { return init_translateX2[i]; },
+    translateY: function(el, i) { return init_translateY2[i]; },
     rotate: 0,
     duration: 0
 }).add({
-    targets: '#Group-41',
-    translateX: 160,
-    translateY: 250,
-    scale: 1/2,
-    rotate: {
-        value: -60,
-        duration: 2500
-    },
+    targets: ['#Group-41', '#Fill-49'],
+    translateX: function(el, i) { return translateX2[i]; },
+    translateY: function(el, i) { return translateY2[i]; },
+    scale: function(el, i) { return scale[i]; },
+    rotate: function(el, i) { return rotate[i]; },
     easing: 'easeInExpo',
-    duration: 2500
+    duration: function(el, i) { return duration[i]; },
 });
 
 document.getElementById("play1").onclick = function() {
@@ -56,5 +62,5 @@ document.getElementById("play1").onclick = function() {
 
 document.getElementById("play2").onclick = function() {
     
-    timeline1.restart();
+    timeline2.restart();
 };
